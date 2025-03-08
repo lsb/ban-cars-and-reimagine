2485a36a3a1455be566adbc1c050d39435d003b3
lsb <leebutterman@gmail.com>
2025-03-08 13:09:23 -0800

    sdxs + canny is very cheap but not good quality

--- modal-server/segformer-sdinpainting.py
+++ modal-server/segformer-sdinpainting.py
@@ -1,21 +1,27 @@
 from io import BytesIO
 from pathlib import Path
-from transformers import SegformerImageProcessor, SegformerForSemanticSegmentation
-from diffusers import AutoPipelineForInpainting
-from PIL import Image
 import torch
+from transformers import SegformerImageProcessor, SegformerForSemanticSegmentation
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
+from skimage.feature import canny
+
+from diffusers import AutoPipelineForInpainting
+from PIL import Image, ImageFilter
 import numpy as np
+import time
 from huggingface_hub import snapshot_download
 
 segformer_model = "nvidia/segformer-b0-finetuned-cityscapes-768-768"
 sdinpaint_model = "stable-diffusion-v1-5/stable-diffusion-inpainting"
-banned_categories = ['car', 'road', 'sidewalk', 'traffic light', 'traffic sign']
+
+ctlnetmodelname = "IDKiro/sdxs-512-dreamshaper-sketch"
+sdmodelname = "IDKiro/sdxs-512-dreamshaper"
 
 
 import modal
 from data_uri_parser import DataURI
 
-container_image = modal.Image.debian_slim().pip_install("diffusers", "transformers", "torch==2.4.1", "accelerate", "data_uri_parser")
+container_image = modal.Image.debian_slim().pip_install("diffusers", "transformers", "torch", "accelerate", "data_uri_parser", "scikit-image") #, "fastapi", "weave")
 
 app = modal.App("segformer-sdinpainting", image=container_image)
 
@@ -25,9 +31,12 @@ class Model:
     def build(self):
         snapshot_download(segformer_model)
         snapshot_download(sdinpaint_model)
+        snapshot_download(ctlnetmodelname)
+        snapshot_download(sdmodelname)
     
     @modal.enter()
     def enter(self):
+        # os.environ['WANDB_API_KEY'] = 'a61467d79ff707c7496c5c9f0a74e510817bb7d2'
         self.segprocessor = SegformerImageProcessor.from_pretrained(
             segformer_model,
             local_files_only=True,
@@ -38,21 +47,36 @@ class Model:
         )
         self.segmodel.to('cuda')
         # self.segmodel.to(torch.float16)
-        self.inpainter =  AutoPipelineForInpainting.from_pretrained(
-            sdinpaint_model,
-            variant='fp16',
-            torch_dtype=torch.float16,
-            local_files_only=True,
+        # self.inpainter =  AutoPipelineForInpainting.from_pretrained(
+        #     sdinpaint_model,
+        #     variant='fp16',
+        #     torch_dtype=torch.float16,
+        #     local_files_only=True,
+        #     safety_checker=None,
+        # )
+        # self.inpainter.to('cuda')
+
+        self.xscontrolnet = ControlNetModel.from_pretrained(
+            ctlnetmodelname, torch_dtype=torch.float16,
         )
-        self.inpainter.to('cuda')
+        self.xscontrolnet.to('cuda')
+        self.xssdmodel = StableDiffusionControlNetPipeline.from_pretrained(
+            sdmodelname, controlnet=self.xscontrolnet, torch_dtype=torch.float16,
+            safety_checker=None)
+        self.xssdmodel.to('cuda')
+
+        # absolutely no self.inpainter.unet = torch.compile(self.inpainter.unet)
+        # absolutely no self.inpainter.vae = torch.compile(self.inpainter.vae)
     
     @modal.web_endpoint(docs=True)
-    def inference(self, image_data_uri, redaction_categories, prompt, size, steps, manual_seed=42069):
+    def inference(self, image_data_uri, redaction_categories, prompt, size, steps, controlnet_conditioning=0, manual_seed=42069):
         with torch.no_grad():
             size = int(size)
             steps = int(steps)
             manual_seed = int(manual_seed)
-            image = Image.open(BytesIO(DataURI(image_data_uri).data)).convert("RGB")
+            controlnet_conditioning = float(controlnet_conditioning)
+            image = Image.open(BytesIO(DataURI(image_data_uri).data)).convert("RGB").resize((768,768))
+            start_time = time.time()
             # purple_image = Image.fromarray(
             #     np.array(image) * np.array([[[1, 0, 1]]], dtype=np.uint8)
             # )
@@ -67,23 +91,47 @@ class Model:
             car_mask = Image.fromarray(
                 torch.tensor(ban_cars_mask, dtype=torch.uint8)[ torch.argmax(logits, dim=0).cpu() ].numpy() * 255
             )
+            # blur the car mask
+            car_mask = car_mask.filter(ImageFilter.GaussianBlur(5))
+            seg_time = time.time() - start_time
+            if False and controlnet_conditioning == 0:
+                inpainted_image = self.inpainter(
+                    image=image,
+                    prompt=prompt,
+                    mask_image=car_mask,
+                    generator=torch.manual_seed(manual_seed),
+                    num_inference_steps=steps,
+                    width=size,
+                    height=size,
+                    guidance_scale=12.0,
+                    strength=1.0,
+                ).images[0]
+            else:
+                image_canny = Image.fromarray(canny(np.array(image.convert("L").resize((size,size))), sigma=1.0).astype(np.uint8) * 255)
+                image_canny_with_mask = Image.fromarray(255 - np.where(
+                    np.array(car_mask.resize((size,size))) == [0],
+                    image_canny,
+                    np.array([255], dtype=np.uint8)))
+                inpainted_image = self.xssdmodel(
+                    prompt=prompt,
+                    image=image_canny_with_mask,
+                    num_inference_steps=1,
+                    guidance_scale=0.0,
+                    controlnet_conditioning_scale=controlnet_conditioning,
+                    generator=torch.manual_seed(manual_seed),
+                    height=size,
+                    width=size,
+                ).images[0]
 
-            inpainted_image = self.inpainter(
-                image=image,
-                prompt=prompt,
-                mask_image=car_mask,
-                generator=torch.manual_seed(manual_seed),
-                num_inference_steps=steps,
-                width=size,
-                height=size,
-                guidance_scale=12.0,
-                strength=1.0,
-            ).images[0]
+            paint_time = time.time() - start_time - seg_time
 
             bs = BytesIO()
-            inpainted_image.save(bs, format="jpeg", subsampling=2)
+            inpainted_image.save(bs, format="jpeg", quality=50, subsampling=2)
             result_image_data_uri = DataURI.from_stream(bs, "x.jpeg")
             return {
                 "image": result_image_data_uri,
                 "redaction_percent": int(np.average(np.array(car_mask) / 255) * 100),
+                "seg_time": seg_time,
+                "paint_time": paint_time,
+                "compiled": "50",
             }
--- src/App.js
+++ src/App.js
@@ -10,21 +10,32 @@ const allCategories = [
 
 const banCarsCategories = ['car', 'road', 'sidewalk', 'traffic light', 'traffic sign', 'person'];
 
-const allSizes = [256, 320, 384, 448, 512, 640, 768, 896, 1024];
+const allSizes = [256, 320, 384, 448, 512, 640, 768, 896, 1024, 2048];
 const allSteps = [8, 12, 16, 20, 50]
+const allConditionings = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0];
+
+const presets = ['urban', 'desert', 'kittens', 'custom'];
 
 const modalEndpoint = "https://lsb--segformer-sdinpainting-model-inference.modal.run/"
+
+const urbanPrompt = "pen and ink watercolor of a leafy pedestrian mall full of people, with multiracial genderqueer joggers and bicyclists and wheelchair users talking and laughing";
+const desertPrompt = "beautiful desert after a rain at midnight, large boulders and pools of water and a dusting of snow";
+const kittensPrompt = "kittens poking their heads out from blankets";
 
 const startingState = {
   imageSrc: null,
   seed: 42,
   size: 640,
-  steps: 12,
+  steps: 94111,
   facingMode: "environment",
-  prompt: "award-winning photo of a leafy pedestrian mall full of people, with multiracial genderqueer joggers and bicyclists and wheelchair users talking and laughing",
+  prompt: urbanPrompt,
   bannedCategories: banCarsCategories,
   maskFraction: 50,
   millis: 0,
+  seg_time: 0,
+  paint_time: 0,
+  conditioning: 0.9,
+  configureExpanded: false,
 }
 
 
@@ -48,7 +59,6 @@ class App extends React.Component {
 
   async capture () {
     if(this.webcamRef && this.webcamRef.current && this.webcamRef.current.getScreenshot) {
-      console.log(this.webcamRef.current.getCanvas());
       const screenshot = this.webcamRef.current.getScreenshot();
       if(screenshot) {
         this.useCapture(screenshot);
@@ -61,7 +71,6 @@ class App extends React.Component {
   }
 
   async useCapture(screenshot) {
-    console.log(screenshot.length);
     const params = new URLSearchParams({
       image_data_uri: screenshot,
       redaction_categories: this.state.bannedCategories.join(","),
@@ -69,34 +78,124 @@ class App extends React.Component {
       size: this.state.size,
       steps: this.state.steps,
       manual_seed: this.state.seed,
+      controlnet_conditioning: this.state.conditioning,
     });
     const startDate = Date.now();
     const response = await fetch(modalEndpoint + "?" + params.toString());
-    const { image, redaction_percent } = await response.json();
+    const { image, redaction_percent, seg_time, paint_time, compiled } = await response.json();
+    console.log(compiled);
     const endDate = Date.now();
-    this.setState({imageSrc: image, rawSnapshot: screenshot, maskFraction: redaction_percent, millis: endDate - startDate});
+    this.setState({imageSrc: image, rawSnapshot: screenshot, maskFraction: redaction_percent, millis: endDate - startDate, seg_time, paint_time});
     requestAnimationFrame(() => this.capture());
   }
 
   render() {
-    const { rawSnapshot, imageSrc, millis, facingMode, maskFraction } = this.state;
+    const { rawSnapshot, imageSrc, size, steps, prompt, millis, facingMode, maskFraction, configureExpanded, seg_time, paint_time } = this.state;
     return (
       <div className="App">
-        <h1>BAN CARS</h1>
-        <div id="maskFraction">The world is at least {maskFraction}% terrible,<br/>though I keep this from my children.</div>
-        <div className="milliseconds">{millis}ms</div>
-        <Webcam
-          audio={false}
-          ref={this.webcamRef}
-          screenshotFormat="image/jpeg"
-          videoConstraints={{...videoConstraints, facingMode}}
-          screenshotQuality={0.5}
-          height={videoConstraints.height}
-          width={videoConstraints.width}
-          style={{"opacity": "0.0001", "position": "absolute", "top": "0px", "right": "0px"}}
-        />
-        <h2><img id="reimagination" src={imageSrc} /></h2>
-        <h2><img id="rawSnapshot" src={rawSnapshot} /></h2>
+      <h1>BAN CARS</h1>
+      <div id="maskFraction">The world is at least {maskFraction}% terrible,<br/>though I keep this from my children.</div>
+      <div className="milliseconds">({Math.round(seg_time * 1000)}/{Math.round(paint_time * 1000)}) {millis}ms</div>
+      <Webcam
+        audio={false}
+        ref={this.webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{...videoConstraints, facingMode}}
+        screenshotQuality={0.5}
+        height={videoConstraints.height}
+        width={videoConstraints.width}
+        style={{"opacity": "0.0001", "position": "fixed", "top": "0px", "right": "0px"}}
+      />
+      <div><img id="reimagination" src={imageSrc} /><img id="rawSnapshot" src={rawSnapshot} /></div>
+      <div>
+        <label>Preset:&nbsp;</label>
+        <select
+          value={(prompt === urbanPrompt) ? 'urban' : (prompt === desertPrompt ? 'desert' : (prompt === kittensPrompt ? 'kittens' : 'custom'))}
+          onChange={(e) => {
+            const selectedPreset = e.target.value;
+            let newPrompt;
+            let newConfigureExpanded;
+            switch (selectedPreset) {
+              case 'urban':
+                newPrompt = urbanPrompt;
+                newConfigureExpanded = false;
+                break;
+              case 'desert':
+                newPrompt = desertPrompt;
+                newConfigureExpanded = false;
+                break;
+              case 'kittens':
+                newPrompt = kittensPrompt;
+                newConfigureExpanded = false;
+                break;
+              case 'custom':
+                newPrompt = this.state.prompt + ' ';
+                newConfigureExpanded = true;
+                break;
+              default:
+                newPrompt = this.state.prompt;
+            }
+            this.setState({ prompt: newPrompt, configureExpanded: newConfigureExpanded });
+          }}
+        >
+          {presets.map((preset) => (
+            <option key={preset} value={preset}>{preset}</option>
+          ))}
+        </select>
+      </div>
+      {/* <button onClick={(e) => this.setState({configureExpanded: !configureExpanded})}>⚙️{configureExpanded ? "🔼" : "🔽"}</button> */}
+      <div id="configure" style={{display: configureExpanded ? "block" : "none", width: "95%", margin: "auto"}}>
+        <div>
+        <label>Prompt:</label><br/>
+        <textarea rows={5} style={{display: "block", width: "95%"}} value={prompt} onChange={(e) => this.setState({prompt: e.target.value})} />
+        </div>
+        <div>
+        <label>Banned Categories:</label>
+        <div>
+          {allCategories.map((category) => (
+          <span key={category}>
+            <input
+            type="checkbox"
+            id={category}
+            value={category}
+            checked={this.state.bannedCategories.includes(category)}
+            onChange={(e) => {
+              const newBannedCategories = e.target.checked
+              ? [...this.state.bannedCategories, category]
+              : this.state.bannedCategories.filter((c) => c !== category);
+              this.setState({ bannedCategories: newBannedCategories });
+            }}
+            />
+            <label htmlFor={category}>{category}</label>
+            &nbsp;
+          </span>
+          ))}
+        </div>
+        </div>
+        {/* <div>
+        <label>Steps: {steps}</label>
+        <input type="range" min={0} max={allSteps.length - 1} step="1" value={allSteps.indexOf(steps)} onChange={(e) => this.setState({steps: allSteps[parseInt(e.target.value)]})} />
+        </div> */}
+        <div>
+        <label>Size: {size}</label>
+        <input type="range" min={0} max={allSizes.length - 1} step="1" value={allSizes.indexOf(size)} onChange={(e) => this.setState({size: allSizes[parseInt(e.target.value)]})} />
+        </div>
+        <div>
+        <label>Conditioning: <code>{this.state.conditioning.toFixed(1)}</code></label>
+        <input type="range" min={0} max={allConditionings.length - 1} step="1" value={allConditionings.indexOf(this.state.conditioning)} onChange={(e) => this.setState({conditioning: allConditionings[parseInt(e.target.value)]})} />
+        </div>
+        <div>
+        <label>Facing Mode:</label>
+        <select value={this.state.facingMode} onChange={(e) => this.setState({facingMode: e.target.value})}>
+          <option value="user">User</option>
+          <option value="environment">Environment</option>
+        </select>
+        </div>
+        <div>
+        <label>Seed:</label>
+        <input type="number" value={this.state.seed} onChange={(e) => this.setState({seed: e.target.value})} />
+        </div>
+      </div>
       </div>
     );
   }
